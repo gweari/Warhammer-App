87af87f3a2b2e073ce3c8a2d6cb263ca7a2bd018
Your Name <your.email@example.com>
2026-01-07 21:42:05 -0600

    Create empty file for accessory sets

---
+++ accessorySetsData.js
@@ -0,0 +1,9 @@
+// Accessory Sets Data
+// This file will contain all accessory sets for all classes
+
+// Add accessory sets to the main gearDatabase
+if (typeof gearDatabase !== 'undefined' && gearDatabase.recommendations) {
+    Object.assign(gearDatabase.recommendations, {
+        // Accessory sets will be added here
+    });
+}
